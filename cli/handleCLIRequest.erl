9196a75c4dd108e4127d3e05846950c2570ea128
William Cordray <William.E.Cordray@gmail.com>
2025-03-24 12:24:41 -0400

    Add beginning of command line support

--- cli/handleCLIRequest.erl
+++ cli/handleCLIRequest.erl
@@ -3,14 +3,25 @@
 %% the basic idea here is that if you run this without the shell, then this can communicate with the client.erl process that each user automatically runs on signin. client.erl can do further processing and contact the main server broker if necessary
 %% assumes some information about the locations of games as well as the location of the clienter.erl process in the defines at the top of the module
 
+%% one weird thing to note is that every function needs to end with 
+%% "init:stop()" to terminate the process
+
 %% run command: erl -pa ebin -noshell -eval "handleCLIRequest:newGame(chess)"
 
+%% TODO: investigate how bad using erlang:halt() is (instead of init:stop()) --> it's much much faster on shutdown, but also does an unclean shutdown which might cause problems.
+
+
 -module(handleCLIRequest).
--export([newGame/1]).
+-export([help/0, newGame/1, joinGame/1, listGames/0]).
 
 -define(GameDir, "games/").
 -define(ClientServer, client).
--define(ClientNode, 'server@vm-hw06')
+-define(ClientNode, 'server@vm-hw06').
+
+% TODO: finish writing usage
+help() ->
+    io:format("Usage:...\n"),
+    init:stop().
 
 newGame(GameName) ->
     {ok, Games} = file:list_dir(?GameDir),
@@ -22,3 +33,27 @@ newGame(GameName) ->
                  error
     end,
     init:stop().
+
+%% TODO
+joinGame(GameID) ->
+    io:format("~p\n", [GameID]),
+    init:stop().
+
+%% TODO
+listGames() ->
+    io:format("(TODO) List games\n"),
+    init:stop().
+
+% % optional
+% addFriend(FriendID) ->
+%     init:stop().
+
+% % optional
+% removeFriend(FriendID) ->
+%     init:stop().
+
+% % this one is also a maybe. The idea is that this could be used to stop the 
+% % register server if the user wants. This function might also be required for 
+% % stopping the server on logout.
+% stopRegisterServer() ->
+%     init:stop().
---
+++ cli/hg.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# this command is the prefix to run the erlang script that handles requests
+script="erl -pa ebin -noshell -eval \"handleCLIRequest:"
+
+# no arguments supplied, or asking for help
+if [ "$#" == 0 ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    eval "${script}help()\""
+    exit 1
+fi
+
+
+if [ "$1" == "join" ]; then
+    if [ "$#" != 2 ]; then
+        echo "Expected a gameID to join"
+        exit 1
+    fi
+    eval "${script}joinGame($2)\""
+elif [ "$1" == "new" ]; then
+    if [ "$#" != 2 ]; then
+        echo "Expected a game name to be supplied"
+        echo "Available Games:" # TODO: the next command should be an ls of the directory holding the games (or something similar)
+        exit 1
+    fi
+    eval "${script}newGame($2)\""
+elif [ "$1" == "games" ]; then
+    eval "${script}listGames()\""
+fi
