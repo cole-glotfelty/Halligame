f9a50ee75b17ec492f2c865b2fcfcce1e2ac761b
Cole Glotfelty <16599506+cole-glotfelty@users.noreply.github.com>
2025-03-30 16:43:36 -0400

    updated devenv: added erpy

--- devenv.nix
+++ devenv.nix
@@ -1,53 +1,19 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  # env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
   packages = with pkgs; [
     git
-    (python39.withPackages (ps: with ps; [ prompt-toolkit ]))
   ];
 
-  # https://devenv.sh/languages/
-  # Same version as halligan
   languages.python = {
     enable = true;
-    version = "3.9.2";
-    venv.enable = true; # This creates a virtual environment
+    version = "3.11";
+    venv = {
+      enable = true;
+      requirements = ''
+        prompt-toolkit
+        erpy
+      '';
+    };
   };
-
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  # scripts.hello.exec = ''
-  #   echo hello from $GREET
-  # '';
-
-  # enterShell = ''
-  #   hello
-  #   git --version
-  # '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  # enterTest = ''
-  #   echo "Running tests"
-  #   git --version | grep --color=auto "${pkgs.git.version}"
-  # '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
