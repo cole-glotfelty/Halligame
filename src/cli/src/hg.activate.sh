5aceeaf70d2e3ff9f4454ecf794c11c60b8b8019
William Cordray <William.E.Cordray@gmail.com>
2025-04-14 18:45:19 -0400

    Update .active files for new python cli

--- src/cli/src/hg.activate.sh
+++ src/cli/src/hg.activate.sh
@@ -1,3 +1,3 @@
-alias hg="$HG_ROOT/src/cli/src/hg.bash"
+alias hg="uv --project $HG_ROOT --directory $HG_ROOT/src/cli_py run cli.py"
 
 # TODO: add in the command that starts the registerclient server
