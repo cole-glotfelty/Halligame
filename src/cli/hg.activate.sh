290230423fc9c0363e9231e159b96d4973630164
Michael Daniels <michael.daniels618738@tufts.edu>
2025-04-19 16:55:21 -0400

    wip

--- src/cli/hg.activate.sh
+++ src/cli/hg.activate.sh
@@ -1,3 +1,3 @@
-alias hg="$HG_ROOT/.venv/bin/python $HG_ROOT/src/cli_py/cli.py"
+alias hg="$HG_ROOT/.venv/bin/python $HG_ROOT/src/cli/cli.py"
 bash $HG_ROOT/src/cli/hg_background.bash
 # TODO: add in the command that starts the registerclient server
