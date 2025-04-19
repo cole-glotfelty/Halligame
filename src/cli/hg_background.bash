7e09bb1f84d0f32efd5067e6bfaac297d943bf88
Michael Daniels <michael.daniels618738@tufts.edu>
2025-04-19 16:36:50 -0400

    fix?

--- src/cli/hg_background.bash
+++ src/cli/hg_background.bash
@@ -3,5 +3,8 @@
 
 export ERL_LIBS="$HG_ROOT/src/user_background/_build/default/lib:$HG_ROOT/src/serverbroker/_build/default/lib:$ERL_LIBS"
 
-$HG_ROOT/.venv/bin/python $HG_ROOT/src/cli/background.py $(ps -o ppid= -p $$) $(tty) &
+HGSHELLPROC=$(ps -o ppid= -p $$)
+HGTTY=$(tty)
+
+$HG_ROOT/.venv/bin/python $HG_ROOT/src/cli/background.py "$HGSHELLPROC" "$HGTTY" &
 disown
