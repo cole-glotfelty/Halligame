46aafc9c5ee1ad13a3c08a2e9ff210ac11fa2901
William Cordray <William.E.Cordray@gmail.com>
2025-04-19 21:57:14 -0400

    Fix more bugs

--- src/halligame/games/Uno/gameServer.py
+++ src/halligame/games/Uno/gameServer.py
@@ -55,11 +55,11 @@ class Server(ServerSuper):
         nextTurnPlayersClientId = self.__clientIdsToClientPids[self.__currUsersTurn]
         if (self.__game.type(card) == "+2"):
             for i in range(2):
-                self.__userCardCounts[nextTurnPlayersClientId][1] += 2
+                self.__userCardCounts[self.__currUsersTurn][1] += 2
                 self.__comms.sendClientMessage(nextTurnPlayersClientId, ("newCard", self.__game.dealCard()))
         elif (self.__game.type(card) == "+4"):
             for i in range(4):
-                self.__userCardCounts[nextTurnPlayersClientId][1] += 4
+                self.__userCardCounts[self.__currUsersTurn][1] += 4
                 self.__comms.sendClientMessage(nextTurnPlayersClientId, ("newCard", self.__game.dealCard()))
 
         if (self.__userCardCounts[userId][1] == 0):
